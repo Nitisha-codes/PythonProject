6e4bb4f70a76f53a1e20ecde9f13889866753185
Nitisha-codes <nitishakarna1@gmail.com>
2026-09-14 17:52:07 +0545

    Organized Day 3 into folder

---
+++ Day03/day02-revision(madlibs game).py
@@ -0,0 +1,17 @@
+# Revising the Day02 activities(lesson)
+# Madlibs game
+# A word game where you create a story
+# by adding or filling the blanks without knowing the context
+
+noun1 = input("Enter a noun (place): ")
+adjective1 = input("Enter an adjective (description of noun): ")
+noun2 = input("Enter a noun (person or animal): ")
+adjective2 = input("Enter another adjective (description of noun): ")
+verb1 = input("Enter a verb (V4) 'ing' : ")
+verb2 = input("Enter another verb (V2/V3) 'd/ed' : ")
+
+print(f"Today,There was a program at {noun1}.")
+print(f"The program location was {adjective1}.")
+print(f"I saw {noun2} at {noun1}.")
+print(f"{noun2} was extremely {adjective2} and {verb1}")
+print(f"I {verb2} it.")
