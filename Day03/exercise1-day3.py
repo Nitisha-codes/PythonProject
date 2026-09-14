0f8574a7290542f6e1bc58e7965b33a0924e76af
Nitisha-codes <nitishakarna1@gmail.com>
2026-09-14 20:28:59 +0545

    Organized Day 3 into folder

---
+++ Day03/exercise1-day3.py
@@ -0,0 +1,8 @@
+# Calculate the circumference of a circle
+# C = 2 * pi * r
+
+import math
+
+r = float(input("Enter the radius of the circle: "))
+circ = math.pi * 2 * r
+print(f"The circumference of the circle is {circ}cm².")
