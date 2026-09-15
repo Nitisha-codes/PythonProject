93c2909733ee989b7b7a6d0ab68eca63436847b1
Nitisha-codes <nitishakarna1@gmail.com>
2026-09-15 17:01:58 +0545

    Organized Day 4 into folder

---
+++ Day04/string_slicing_(slice).py
@@ -0,0 +1,12 @@
+# Slice : built-in Python function that creates a reusable slice object.
+# (start,stop,step)
+
+website1 = "https://www.google.com"
+slice = slice(12,-4)
+print(website1[slice])
+
+website2 = "https://www.github.com"
+print(website2[slice])
+
+
+
