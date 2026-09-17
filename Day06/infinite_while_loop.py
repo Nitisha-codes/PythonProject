588708ea38e8a6a7ec963bc890861637a0b63cde
Nitisha-codes <nitishakarna1@gmail.com>
2026-09-17 19:20:08 +0545

    Organized Day 6 into folder

---
+++ Day06/infinite_while_loop.py
@@ -0,0 +1,6 @@
+# While loop : a control flow statement used to repeatedly execute a block of code as long as a specified condition remains True
+
+# An infinite loop
+
+while 1==1:
+    print("Welcome to my Git!")
