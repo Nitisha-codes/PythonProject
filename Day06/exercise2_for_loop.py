705a05467e5dcbb71c4e8fa21bef338101c0542a
Nitisha-codes <nitishakarna1@gmail.com>
2026-09-17 19:43:08 +0545

    Organized Day 6 into folder

---
+++ Day06/exercise2_for_loop.py
@@ -0,0 +1,10 @@
+# Create a 'Happy New Year' countdown
+
+import time
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+
+print("Happy New Year!!")
+print("Akemashita omedeto!!!!")
---
+++ Day06/for_loop.py
@@ -0,0 +1,22 @@
+# for loop : a control flow statement used to iterate over a sequence (like a list, tuple, dictionary, set, or string) or any other iterable object
+# for loop = finite ( limited )
+# while loop = infinite ( unlimited )
+
+
+# Numbers from 1 to 10
+for i in range(10):
+    print(i+1)
+
+# Numbers from 50 to 100
+for j in range(50,100+1):
+    print(j)
+
+# Even numbers from 50 to 100
+for k in range(20,100+1,2):
+    print(k)
+
+# Each letter in the string in order
+for l in "Nitisha Karna":
+    print(l)
+
+    
